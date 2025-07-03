e98b6bc0ce2f9aa183c8e593040f59bbb6b51979
Raghuvanshi4344-CS <priyansh0975@gmail.com>
2025-07-03 23:15:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.java
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -1,9 +1,7 @@
 class Solution {
     public static int findSum(int n) {
         // code here
-        if(n==0){
-            return 0;
-        }
+        if(n==0) return 0;
         return n=n+findSum(n-1);
     }
 }
