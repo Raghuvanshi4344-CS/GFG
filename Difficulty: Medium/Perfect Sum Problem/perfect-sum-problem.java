76f1583d1a93b21c85e664bd7288eefe9f229056
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2025-07-16 12:37:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -5,9 +5,10 @@ class Solution {
         int n=nums.length;
         int m=target;
         int arr[][]=new int[n+1][target+1];
-        for(int i=0;i<=n;i++){
-            arr[i][0]=1;
-        }
+        // for(int i=0;i<=n;i++){
+        //     arr[i][0]=1;
+        // }
+        arr[0][0]=1;
         for(int i=1;i<=n;i++){
             for(int j=0;j<=m;j++){
                 if(nums[i-1]>j){
